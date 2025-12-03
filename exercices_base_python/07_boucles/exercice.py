5b61aac10727a94966be2c7dcbacb67d48891968
Nadia <pe78iom@eduvaud.ch>
2025-12-03 12:31:52 +0100

    Exercice 7 complété

--- exercices_base_python/07_boucles/exercice.py
+++ exercices_base_python/07_boucles/exercice.py
@@ -4,7 +4,16 @@
 
 def somme_1_a_n(n):
     # TODO
-    return None
+
+
+    for i in range(1, 9):
+        secret = 8
+        essai = None
+    while essai != secret:
+        essai = int(input("Devine: "))
+    print("Bravo!")
+
 
 if __name__ == "__main__":
     print("Somme 1..5 =", somme_1_a_n(5))
+
