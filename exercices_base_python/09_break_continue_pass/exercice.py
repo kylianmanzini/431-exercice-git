b554e97dfb2ff604bd58be6e5c9aa021fa3092bd
Nadia <pe78iom@eduvaud.ch>
2025-12-03 15:03:31 +0100

    Exercice 9 complété

--- exercices_base_python/09_break_continue_pass/exercice.py
+++ exercices_base_python/09_break_continue_pass/exercice.py
@@ -1,14 +1,24 @@
 # 09 — break, continue, pass
 
 # TODO: illustrer break/continue/pass
-
+nums=[1,3,5,8,9]
 def rechercher_premier_pair(nums):
     # TODO: utiliser continue et break
-    return None
 
-def a_implementer_plus_tard():
+    for i in nums:
+        if  i % 2 != 0: # si impair
+            continue    #on passe au suivant
+        #on arrive ici si i est pair
+        return i # retourne le premier pair
+        break # arrête la boucle mais inutile car return arrete la boucle déjà
+
+
+
+def a_implementer_plus_tard(nums):
     # TODO: laisser pass
     pass
 
+
+
 if __name__ == "__main__":
-    print(rechercher_premier_pair([1,3,5,8,9]))
+    print(rechercher_premier_pair(nums))
