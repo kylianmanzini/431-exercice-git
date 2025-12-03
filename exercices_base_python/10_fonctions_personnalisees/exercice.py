2876fd5bc86284f31da5b1c1833f0c1ff9e2d590
CamBach <camille.bachmann.2005@gmail.com>
2025-12-03 11:35:32 +0100

    ex 10

--- exercices_base_python/10_fonctions_personnalisees/exercice.py
+++ exercices_base_python/10_fonctions_personnalisees/exercice.py
@@ -3,12 +3,12 @@
 # TODO: définir aire_rectangle(l, h) et saluer(nom)
 
 def aire_rectangle(l, h):
-    # TODO
-    return None
+    aire = l * h
+    return aire
 
 def saluer(nom):
-    # TODO: retourner une chaîne
-    return None
+    salutation = "Bonjour " + nom + "!"
+    return salutation
 
 if __name__ == "__main__":
     print(aire_rectangle(3, 4))
