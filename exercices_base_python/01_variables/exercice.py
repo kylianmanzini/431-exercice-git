bfe9012ea6bcdb84c959b325b641edfc090bb35d
Ruben Ten Cate <pv34nju@eduvaud.ch>
2025-12-03 11:34:39 +0100

    Exercice 1 validé

--- exercices_base_python/01_variables/exercice.py
+++ exercices_base_python/01_variables/exercice.py
@@ -1,13 +1,29 @@
 # 01 — Variables
 # TODO: compléter les affectations et manipulations demandées dans README.md
+# Le mot clé TODO est un marqueur de tâche dans les commentaires, il est affiché par l'interpréteur et met en évidence.
 
-a_entier = None  # TODO: un int
-b_flottant = None  # TODO: un float
-c_chaine = None  # TODO: une str
-liste = None  # TODO: une list
-mon_tuple = None  # TODO: un tuple
-mon_dict = None  # TODO: un dict
 
-if __name__ == "__main__":
-    print("Valeurs:", a_entier, b_flottant, c_chaine, liste, mon_tuple, mon_dict)
+a_entier = 5  # TODO: un int
+b_flottant = 5.5  # TODO: un float
+c_chaine = "elephant"  # TODO: une str
+
+# une list (les crochets [] sont utilisés pour les listes)
+
+liste =  ["numero1", "numero2", "numero3"]# TODO: une list
+
+# un tuple (les parenthèses () sont utilisées pour les tuples)
+
+mon_tuple = ("valeur1, valeur2")  # TODO: un tuple
+
+# un dict (les accolades {} avec des paires clé:valeur)
+
+mon_dict = {"object": "velo",
+            "couleur": "rouge",
+            "prix": 120,
+            "en_stock":True}  # TODO: un dict
+
+if __name__ == "__main__": #if __name__ (c'est a dire : si ce nom du fichier) == (est égal a) "__main__" (le nom programme principal)
+    print(f"Valeurs: {a_entier}, {b_flottant}, {c_chaine}, {liste}, {mon_tuple}, {mon_dict}")
     print("Types:", type(a_entier), type(b_flottant), type(c_chaine), type(liste), type(mon_tuple), type(mon_dict))
+
+
