6dbaec9abe0ea3af5098839a22cbcbee2071d141
Ruben ten Cate <tencateruben@gmail.com>
2025-12-03 12:16:52 +0100

    Exercice 4 Validé

--- exercices_base_python/04_operateurs_comparaison/exercice.py
+++ exercices_base_python/04_operateurs_comparaison/exercice.py
@@ -4,10 +4,11 @@ x = 10
 y = 5
 
 # TODO: compléter
-egal = None
-different = None
-plus_grand = None
-logique = None  # exemple: (x > y) and (y > 0)
+egal = x == y
+different = x != y
+plus_grand = x > y
+logique = (x>y) and (y>5)  # and signiqfie que les deux conditions doivent être vraies pour obtenir True
+logique2 = (x<y) or (y!=4) # or signifie que une des deux conditions doit être vraie pour obtenir True
 
 if __name__ == "__main__":
-    print(egal, different, plus_grand, logique)
+    print(egal, different, plus_grand, logique, logique2)
