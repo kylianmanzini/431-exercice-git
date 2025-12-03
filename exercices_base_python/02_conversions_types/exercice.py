2cb4c5aa4af42f9a660a29c075acad754634eae6
CamBach <camille.bachmann.2005@gmail.com>
2025-12-03 11:05:57 +0100

    ex 2 fini

--- exercices_base_python/02_conversions_types/exercice.py
+++ exercices_base_python/02_conversions_types/exercice.py
@@ -5,9 +5,9 @@ val_str_num = "42"
 val_str_alpha = "abc"
 
 # TODO: convertir en int, float, str selon les consignes
-res_int = None
-res_float = None
-res_str = None
+res_int = int(val_str_num)
+res_float = float(val_str_num)
+res_str = str(val_float)
 
 if __name__ == "__main__":
     print("Conversions:", res_int, res_float, res_str)
