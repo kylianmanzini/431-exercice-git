917a1cc3c7141372bda776024c9c53a3c3cfd965
Farah Mohamed Ahmed <ps51mdf@eduvaud.ch>
2025-12-03 11:48:04 +0100

    exercice 1

--- exercices_base_python/01_variables/exercice.py
+++ exercices_base_python/01_variables/exercice.py
@@ -1,12 +1,12 @@
 # 01 — Variables
 # TODO: compléter les affectations et manipulations demandées dans README.md
 
-a_entier = None  # TODO: un int
-b_flottant = None  # TODO: un float
-c_chaine = None  # TODO: une str
-liste = None  # TODO: une list
-mon_tuple = None  # TODO: un tuple
-mon_dict = None  # TODO: un dict
+a_entier = 10  # TODO: un int
+b_flottant = 4.5  # TODO: un float
+c_chaine = "blabla"  # TODO: une str
+liste = [1,2,3,4]  # TODO: une list
+mon_tuple = (10,20,40)  # TODO: un tuple
+mon_dict = {"nom": "Alice", "age":20} # TODO: un dict
 
 if __name__ == "__main__":
     print("Valeurs:", a_entier, b_flottant, c_chaine, liste, mon_tuple, mon_dict)
