fe48209eb5e1977f9e072fce3212db11adfb6287
Nadia <pe78iom@eduvaud.ch>
2025-12-03 15:02:05 +0100

    Exercice 8 complété

--- exercices_base_python/08_fonctions_primitives/exercice.py
+++ exercices_base_python/08_fonctions_primitives/exercice.py
@@ -4,11 +4,17 @@
 
 data = [3, -1, 7]
 res = {
-    "abs_-1": None,
-    "len_data": None,
-    "max_data": None,
-    "type_tuple": None,
+    "abs_-1": abs(-1),
+    "len_data": len(data),
+    "max_data": max(data),
+    "type_tuple": type(data),
+    "range_data": range(-1,8) ,
+    "round": round(3.145259) ,
+    "str_data": str(data),
+    "bool_data": bool(data)
 }
+print(data)
+
 
 if __name__ == "__main__":
     print(res)
