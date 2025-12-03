0fe909df63398ab560db6aa41e70f5d3bc90d045
Nadia <pe78iom@eduvaud.ch>
2025-12-03 15:09:08 +0100

    Exercice 15 complété

--- exercices_base_python/15_commentaires_portee/exercice.py
+++ exercices_base_python/15_commentaires_portee/exercice.py
@@ -3,8 +3,14 @@
 # TODO: ajouter des commentaires pertinents dans les fonctions ci-dessous
 
 def somme(a, b):
-    # TODO
+
+    #Paramètres
+    #a: premier nombre
+    #b: second nombre
     return None
+    #Retourne la somme de a et b
 
 if __name__ == "__main__":
+    #Exemple d'utilisation
     print(somme(2, 3))
+    #Affiche le résultat de la somme: %
