6816ae05149e314576348c2f07a89677a27b21fe
pi47rju <arthur.saugy@eduvaud.ch>
2025-12-03 10:35:51 +0100

    Partie somme for de l'exercice 7

--- exercices_base_python/07_boucles/exercice.py
+++ exercices_base_python/07_boucles/exercice.py
@@ -3,8 +3,10 @@
 # TODO: implémenter une somme avec for et un petit jeu avec while.
 
 def somme_1_a_n(n):
-    # TODO
-    return None
+    somme=0
+    for i in range(n+1):
+        somme+=i
+    return somme
 
 if __name__ == "__main__":
     print("Somme 1..5 =", somme_1_a_n(5))
