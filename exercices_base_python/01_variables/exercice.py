17fd2242404701ea182d5f37e6ea6457cea4024c
Aleksander Johnson <pn42yut@eduvaud.ch>
2025-12-03 11:29:22 +0100

    test

--- exercices_base_python/01_variables/exercice.py
+++ exercices_base_python/01_variables/exercice.py
@@ -11,3 +11,5 @@ mon_dict = None  # TODO: un dict
 if __name__ == "__main__":
     print("Valeurs:", a_entier, b_flottant, c_chaine, liste, mon_tuple, mon_dict)
     print("Types:", type(a_entier), type(b_flottant), type(c_chaine), type(liste), type(mon_tuple), type(mon_dict))
+
+#je teste des trucs!!!
