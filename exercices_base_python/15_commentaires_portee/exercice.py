0e26c291939401c6922c3a0c02d8cef17c869918
pi47rju <arthur.saugy@eduvaud.ch>
2025-12-03 10:58:46 +0100

    Résolutions exercice 15

--- exercices_base_python/15_commentaires_portee/exercice.py
+++ exercices_base_python/15_commentaires_portee/exercice.py
@@ -3,8 +3,19 @@
 # TODO: ajouter des commentaires pertinents dans les fonctions ci-dessous
 
 def somme(a, b):
-    # TODO
-    return None
+    """
+    
+    Fonction qui calcule la somme de 2 nombres
+    
+    :param a: premier nombre
+    :param b: deuxième nombre
+    """
+    
+    # Vérifier si les paramètres sont bien des nombres (entiers, flottants, complexes)
+    if isinstance(a, (int, float, complex)) and isinstance(a, (int, float, complex)):
+        # Faire la somme et retourner le résultat
+        return a + b
+
 
 if __name__ == "__main__":
     print(somme(2, 3))
