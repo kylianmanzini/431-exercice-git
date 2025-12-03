95c62a9866e09fe6c0deed2c9564bf3a4254758d
Nadia <pe78iom@eduvaud.ch>
2025-12-03 15:06:27 +0100

    Exercice 12 complété

--- exercices_base_python/12_fstrings/exercice.py
+++ exercices_base_python/12_fstrings/exercice.py
@@ -5,7 +5,7 @@ age = 20
 annee_courante = 2025
 
 # TODO: construire une chaîne formatée avec f-string
-message = None
+message = f"{nom} a {age} ans et est née {annee_courante}."
 
 if __name__ == "__main__":
     print(message)
