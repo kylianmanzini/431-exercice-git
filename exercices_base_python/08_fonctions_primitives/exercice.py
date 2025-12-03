45bb621ec4d9042419898eabb5e13d6b5d8097ca
CamBach <camille.bachmann.2005@gmail.com>
2025-12-03 11:27:12 +0100

    ex 8

--- exercices_base_python/08_fonctions_primitives/exercice.py
+++ exercices_base_python/08_fonctions_primitives/exercice.py
@@ -4,10 +4,10 @@
 
 data = [3, -1, 7]
 res = {
-    "abs_-1": None,
-    "len_data": None,
-    "max_data": None,
-    "type_tuple": None,
+    "abs_-1": abs(-1),
+    "len_data": len(data),
+    "max_data": max(data),
+    "type_tuple": tuple(data),
 }
 
 if __name__ == "__main__":
