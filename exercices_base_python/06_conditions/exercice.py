7ab8c70e1d9139a38845626f4c8ff9fed81268fc
pi47rju <arthur.saugy@eduvaud.ch>
2025-12-03 10:33:20 +0100

    Résolution exercice 6

--- exercices_base_python/06_conditions/exercice.py
+++ exercices_base_python/06_conditions/exercice.py
@@ -3,5 +3,12 @@
 # TODO: lire une note (float) et afficher la mention avec if/elif/else.
 
 if __name__ == "__main__":
-    note = None
-    # TODO
+    note = 6
+    if (1 <= note) and (note < 4):
+        print("Insuffisant")
+    elif (4 <= note) and (note < 5.5):
+        print("Moyen")
+    elif (5.5 <= note) and (note <= 6):
+        print("Bien")
+    else:
+        print("Votre note n'est pas entre 1 et 6")
