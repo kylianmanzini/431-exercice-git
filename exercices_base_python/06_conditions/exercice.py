c2131671930206cb554dcb03fe2207c944136454
CamBach <camille.bachmann.2005@gmail.com>
2025-12-03 11:18:43 +0100

    ex 6

--- exercices_base_python/06_conditions/exercice.py
+++ exercices_base_python/06_conditions/exercice.py
@@ -3,5 +3,12 @@
 # TODO: lire une note (float) et afficher la mention avec if/elif/else.
 
 if __name__ == "__main__":
-    note = None
-    # TODO
+    note = 5.5
+    if note == 6:
+        print("Excellent")
+    elif note >= 5:
+        print("Bien")
+    elif note >= 4:
+        print("Assez bien")
+    else:
+        print("Insuffisant")
