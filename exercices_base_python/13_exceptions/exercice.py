9fb44433156206d1c6924918d4d9f11989119aa9
Nadia <pe78iom@eduvaud.ch>
2025-12-03 15:06:56 +0100

    Exercice 13 complété

--- exercices_base_python/13_exceptions/exercice.py
+++ exercices_base_python/13_exceptions/exercice.py
@@ -2,9 +2,15 @@
 
 # TODO: entourer de try/except
 
-def convertir_en_int(s):
-    # TODO: retourner un int ou None sur erreur
-    return None
+def convertir_en_int(abc):
+    try:
+        return int(abc)
+    except ValueError:
+        print("Conversion invalide:", abc)
+        return None
+    finally:
+        pass  # code toujours exécuté
+
 
 if __name__ == "__main__":
     print(convertir_en_int("123"))
