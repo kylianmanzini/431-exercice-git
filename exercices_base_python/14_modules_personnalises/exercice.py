707e8518c9e4f70ed0f435b9892787ad4603dc35
pi47rju <arthur.saugy@eduvaud.ch>
2025-12-03 10:53:57 +0100

    Résolution exercice 14 + Création librairie outils.py

--- exercices_base_python/14_modules_personnalises/exercice.py
+++ exercices_base_python/14_modules_personnalises/exercice.py
@@ -1,8 +1,7 @@
 # 14 — Modules personnalisés
 
-# TODO: créer un fichier outils.py avec carre(x) et l'importer ici
 
 if __name__ == "__main__":
-    # from outils import carre
-    # print(carre(5))
+    import outils
+    print(outils.carre(5))
     pass
