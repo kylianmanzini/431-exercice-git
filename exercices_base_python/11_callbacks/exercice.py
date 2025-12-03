2bc5426582e745793d6364c4c97848a202a229eb
pi47rju <arthur.saugy@eduvaud.ch>
2025-12-03 10:48:42 +0100

    Résolution exercice 11

--- exercices_base_python/11_callbacks/exercice.py
+++ exercices_base_python/11_callbacks/exercice.py
@@ -3,8 +3,10 @@
 # TODO: implémenter appliquer(nums, f)
 
 def appliquer(nums, f):
-    # TODO
-    return None
+    new_list = []
+    for i in nums:
+        new_list.append(f(i))
+    return new_list
 
 if __name__ == "__main__":
     print(appliquer([1,2,3], lambda x: x * 2))
