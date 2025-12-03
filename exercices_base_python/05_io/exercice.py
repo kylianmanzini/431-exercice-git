301f8654d4ae9ebb0e583fd14619304ae3824d95
Nadia <pe78iom@eduvaud.ch>
2025-12-03 11:48:33 +0100

    Exercice 5 complété

--- exercices_base_python/05_io/exercice.py
+++ exercices_base_python/05_io/exercice.py
@@ -2,8 +2,10 @@
 
 # TODO: demander un nom et un âge (avec input), convertir l'âge en int, puis afficher.
 
-if __name__ == "__main__":
-    nom = None
-    age = None
-    # TODO
-    print("Bonjour", nom, "— âge:", age)
+# TODO
+nom = input("Quel est votre nom: ")
+age = int(input("Quel est votre âge: "))
+
+
+# TODO
+print("Bonjour", nom, "— âge:", age)
