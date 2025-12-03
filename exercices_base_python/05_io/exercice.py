4502583d582023c666bec9f4584c4387342f5ea5
pi47rju <arthur.saugy@eduvaud.ch>
2025-12-03 10:29:08 +0100

    Résolution exercice 5

--- exercices_base_python/05_io/exercice.py
+++ exercices_base_python/05_io/exercice.py
@@ -3,7 +3,11 @@
 # TODO: demander un nom et un âge (avec input), convertir l'âge en int, puis afficher.
 
 if __name__ == "__main__":
-    nom = None
-    age = None
+    try: 
+        nom = str(input("Votre nom svp : "))
+        age = int(input("Votre age svp : "))
+    except ValueError:
+        print("Erreur : Vous avez mis une mauvaise valeur. ")
+        quit()
     # TODO
     print("Bonjour", nom, "— âge:", age)
