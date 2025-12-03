d3622d161ea56bea6c0cace461fbf8789ab0d36e
CamBach <camille.bachmann.2005@gmail.com>
2025-12-03 11:24:49 +0100

    ex 7

--- exercices_base_python/07_boucles/exercice.py
+++ exercices_base_python/07_boucles/exercice.py
@@ -3,8 +3,18 @@
 # TODO: implémenter une somme avec for et un petit jeu avec while.
 
 def somme_1_a_n(n):
-    # TODO
-    return None
+    somme = 0
+    for i in range(1, n + 1):
+        somme += i
+    return somme
 
 if __name__ == "__main__":
     print("Somme 1..5 =", somme_1_a_n(5))
+    boucle = True
+    while boucle:
+        entree = input("Entrez 'q' pour quitter, et autre chose pour jouer : \n")
+        if entree == 'q':
+            print("Jeu terminé.")
+            boucle = False
+        else:
+            print(f"Vous avez entré : {entree}. Rejouez ! (Oui le jeu est nul.)")
