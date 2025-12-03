6d34db89bbdd81d759e90095ae54ceae0f1d2c48
CamBach <camille.bachmann.2005@gmail.com>
2025-12-03 11:14:10 +0100

    ex 5

--- exercices_base_python/05_io/exercice.py
+++ exercices_base_python/05_io/exercice.py
@@ -3,7 +3,7 @@
 # TODO: demander un nom et un âge (avec input), convertir l'âge en int, puis afficher.
 
 if __name__ == "__main__":
-    nom = None
-    age = None
+    nom = input("Entrez votre nom : \n")
+    age = int(input("Entrez votre âge : \n"))
     # TODO
     print("Bonjour", nom, "— âge:", age)
